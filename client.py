aee116a4e7cb67e08d9ec2bb67904a7ce7b2e362
Wyatt Marks <inbox@wyattmarks.com>
2022-09-26 22:44:42 -0500

    Add the option to encrypt channels. Currently public keys are just exchanged at runtime - it would probably be safer to exchange them once and verify they don't change (for both server _and_ client)

--- client.py
+++ client.py
@@ -2,13 +2,14 @@
 #  This is the client implmentation. 
 #  You could get away with a simple telnet connection - but then you have to specify /chat for sending messages
 #  NARC - Not Another Relay Chat 
+#  https://github.com/wyattmarks/narc
 
 import socket
 import threading
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-import random
+from Crypto import Random
 
 ## Client object for the client side
 #  Simply sends the commands you give it to the server
@@ -22,6 +23,13 @@ class Client:
 
 		self.waiting_for_password = False
 
+		randomGenerator = Random.new().read
+		self.rsaKey = RSA.generate(1024, randomGenerator)
+		self.decryptor = PKCS1_OAEP.new(self.rsaKey)
+		self.channel = None
+		self.channel_encrypted = False
+		
+
 	def connect(self):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.connect((self.ip, self.port))
@@ -32,6 +40,9 @@ class Client:
 		while True:
 			try:
 				response = self.socket.recv(4200)
+				if response[0:10].decode() == "ENCRYPTED:":
+					response = self.decryptor.decrypt(response[10:])
+					self.channel_encrypted = True
 
 				if response.decode().startswith(f"{self.nick} is now known as ") or response.decode().startswith("Welcome back, "):
 					self.nick = response.decode().strip().replace(f"{self.nick} is now known as ", "").replace("Welcome back, ", "")
@@ -44,6 +55,9 @@ class Client:
 						sys.stdout.write('\033[2K\033[1G')
 						print("Password: ", end="")
 						sys.stdout.flush()
+					elif response.decode().startswith("PUBKEYREQ"):
+						self.socket.send(self.rsaKey.publickey().exportKey())
+						self.channel_encrypted = True
 					else:
 						sys.stdout.write('\033[2K\033[1G') #Get rid of the <user> in console from the input() call
 						print(response.decode().strip())
@@ -66,6 +80,12 @@ class Client:
 				print(f"<{self.nick}> ", end="") 
 				sys.stdout.flush()
 
+	def send(self, message):
+		if self.channel_encrypted:
+			self.socket.send("ENCRYPTED:".encode() + self.encryptor.encrypt(message))
+		else:
+			self.socket.send(message)
+
 	def handle_input(self):
 		while True:
 			try:
@@ -76,12 +96,16 @@ class Client:
 						self.socket.send(password)
 						self.waiting_for_password = False
 					elif message.startswith("/"): #If it starts with / then it is a command, and we should not prepend /chat
+						if message.startswith("/channel #"):
+							self.channel = message.replace("/channel ", "")
+							self.channel_encrypted = False
+
+						self.send(f"{message} \r\n".encode())
+
 						if message.startswith("/quit") or message.startswith("/exit"):
 							raise KeyboardInterrupt #hacky way of making it exit, lol
-
-						self.socket.send(f"{message} \r\n".encode())
 					else:
-						self.socket.send(f"/chat {message}\r\n".encode()) #if we don't specify a command, assume that it should be a chat message
+						self.send(f"/chat {message}\r\n".encode()) #if we don't specify a command, assume that it should be a chat message
 			except KeyboardInterrupt:
 				print("\nDisconnecting...")
 				break
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 ## Super simple relay chat protocol.
 #  This is the server implmentation. 
 #  NARC - Not Another Relay Chat 
+#  https://github.com/wyattmarks/narc
 
 
 import socket
@@ -45,7 +46,10 @@ class Client:
 			"/online": self.online,
 			"/help": self.help,
 			"/claim": self.claim,
+			"/encrypt": self.encrypt_channel,
 		}
+
+		self.encryptor = None
 
 	## Start the connection. Blocks until connection ends
 	def begin(self):
@@ -56,19 +60,25 @@ class Client:
 
 		while self.connected:
 			try:
-				msg = self.socket.recv(4096).decode()
+				msg = self.socket.recv(4096)
+				if msg[0:10].decode() == "ENCRYPTED:":
+					msg = self.server.decryptor.decrypt(msg[10:])
+				msg = msg.decode()
 				
 				print(f"{self.nick} ({self.ip})> {msg.strip()}")
 
-				valid = False
-				for command in self.commands:
-					if msg.startswith(command + " "):
-						self.commands[command](msg.strip().replace(command + " ", "")) #Call the callback for this command from the dictionary
-						valid = True
-						break
-				
-				if not valid:
-					self.socket.send("Error, command not found.\r\n".encode())
+				if msg.startswith("-----BEGIN PUBLIC KEY-----"):
+					self.encryptor = PKCS1_OAEP.new(RSA.importKey(msg))
+				else:
+					valid = False
+					for command in self.commands:
+						if msg.startswith(command + " "):
+							self.commands[command](msg.strip().replace(command + " ", "")) #Call the callback for this command from the dictionary
+							valid = True
+							break
+					
+					if not valid:
+						self.send("Error, command not found.\r\n".encode())
 
 			except BrokenPipeError: #Broken pipe means that client disconnected, let people know
 				print(f"{self.nick} has disconnected..")
@@ -80,28 +90,48 @@ class Client:
 			
 		self.socket.close() #close the socket after the infinite loop ends
 
+	def send(self, bytes):
+		if self.channel is not None and self.server.is_channel_encrypted(self.channel):
+			if self.encryptor is None:
+				self.request_pubkey() #shouldnt happen, but eh? ask again I guess
+			else:
+				self.socket.send("ENCRYPTED:".encode() + self.encryptor.encrypt(bytes))
+		else:
+			self.socket.send(bytes)
+
+	def request_pubkey(self):
+		self.send("PUBKEYREQ\r\n".encode())
+
 	## Join the given channel
 	def join_channel(self, msg):
 		if msg.startswith("#"): # Just like IRC, channels start with #
-			self.socket.send(f"Joining {msg}...\r\n".encode())
+			if self.server.is_channel_encrypted(msg):
+				if self.authed:
+					if self.encryptor is None:
+						self.request_pubkey()
+				else:
+					self.send("You must be registered to join this channel because it is encrypted.\r\n".encode())
+					return
+
+			self.send(f"Joining {msg}...\r\n".encode())
 			self.server.arrival(self, msg) # Tell anyone in the channel someone joined
 			self.server.departure(self) # Tell anyone in the old channel that someone left
 			self.channel = msg
 		else:
-			self.socket.send(f"Channel names must begin with a #\r\n".encode())
+			self.send(f"Channel names must begin with a #\r\n".encode())
 		
 	## Broadcast a message to the channel the client is in
 	def send_chat(self, msg):
 		if self.channel != None:
 			self.server.chat(self, msg) # Broadcast the chat message in this channel
 		else:
-			self.socket.send("You must join a channel first (/join)\r\n".encode())
+			self.send("You must join a channel first (/join)\r\n".encode())
 
 	## Set the client's nick name
 	def set_nick(self, msg):
 		if msg != "/nick":
 			if self.server.is_nick_registered(msg):
-				self.socket.send(f"Sorry, {msg} is registered. If this is your nick, use /auth <nick> to login\r\n".encode())
+				self.send(f"Sorry, {msg} is registered. If this is your nick, use /auth <nick> to login\r\n".encode())
 				return
 
 			if self.server.nick_available(msg):
@@ -109,44 +139,44 @@ class Client:
 				self.server.broadcast(self.channel, f"{self.nick} is now known as {msg}!")
 				self.nick = msg
 			else:
-				self.socket.send(f"Sorry, {msg} is already in use.\r\n".encode())
+				self.send(f"Sorry, {msg} is already in use.\r\n".encode())
 		else:
-			self.socket.send("You must give a nickname to use after /nick\r\n".encode())
+			self.send("You must give a nickname to use after /nick\r\n".encode())
 
 	## The the current channel's nick name
 	def set_motd(self, msg):
 		if msg == "/motd":
-			self.socket.send((self.server.get_motd(self.channel) + "\r\n").encode())
+			self.send((self.server.get_motd(self.channel) + "\r\n").encode())
 			return
 		
 		if self.channel is None:
-			self.socket.send("You must be a channel to see a channel's MOTD..\r\n".encode())
+			self.send("You must be a channel to see a channel's MOTD..\r\n".encode())
 			return
 
 		if not self.server.is_channel_claimed(self.channel):
-			self.socket.send("This channel is not claimed, and therefore has no MOTD. Claim it with /claim!\r\n".encode())
+			self.send("This channel is not claimed, and therefore has no MOTD. Claim it with /claim!\r\n".encode())
 			return
 
 		if not self.authed:
-			self.socket.send((self.server.get_motd(self.channel) + "\r\n").encode())
+			self.send((self.server.get_motd(self.channel) + "\r\n").encode())
 			return
 
 		if self.nick != self.server.get_channel_owner(self.channel):
-			self.socket.send((self.server.get_motd(self.channel) + "\r\n").encode())
+			self.send((self.server.get_motd(self.channel) + "\r\n").encode())
 			return
 		
 		self.server.set_motd(self, msg)
-		self.socket.send("Channel MOTD set.\r\n".encode())
+		self.send("Channel MOTD set.\r\n".encode())
 
 			
 
 	## Ping
 	def pong(self, msg):
-		self.socket.send("Pong!\r\n".encode())
+		self.send("Pong!\r\n".encode())
 
 	## End the connection
 	def quit(self, msg):
-		self.socket.send("Goodbye!\r\n".encode())
+		self.send("Goodbye!\r\n".encode())
 		self.server.departure(self)
 		self.connected = False
 
@@ -157,16 +187,16 @@ class Client:
 			message = f"There are currently {len(clients)} people online in {self.channel}\r\n"
 			for c in clients:
 				message += f"\t{c.nick}\r\n"
-			self.socket.send(message.encode())
+			self.send(message.encode())
 
 		else:
-			self.socket.send("Join a channel to see who's online in that channel\r\n".encode())
+			self.send("Join a channel to see who's online in that channel\r\n".encode())
 
 	def list_channels(self, msg):
 		message = f"There are currently {len(self.server.channels)} registered channels\r\n"
 		for c in self.server.channels:
 			message += f"\t{c}\r\n"
-		self.socket.send(message.encode())
+		self.send(message.encode())
 
 
 	## List the command dictionary
@@ -175,59 +205,86 @@ class Client:
 		for cmd in self.commands:
 			message += f"\t{cmd}\r\n"
 
-		self.socket.send(message.encode())
+		self.send(message.encode())
 
 	## Login with password
 	def auth(self, msg):
 		if msg == "/auth":
-			self.socket.send("You must provide a nickname to login to\r\n".encode())
+			self.send("You must provide a nickname to login to\r\n".encode())
 			return
 
 		if not self.server.is_nick_registered(msg):
-			self.socket.send("This nickname is not registered\r\n".encode())
+			self.send("This nickname is not registered\r\n".encode())
 			return
 
 		if self.authed:
-			self.socket.send("You are already logged in..\r\n".encode())
+			self.send("You are already logged in..\r\n".encode())
 			return
 
 
-		self.socket.send(self.server.rsaKey.publickey().exportKey())
+		self.send(self.server.rsaKey.publickey().exportKey())
 		if self.server.auth(msg, self.server.decryptor.decrypt(self.socket.recv(4096).strip()) + self.server.get_salt(msg).encode()):
 			self.nick = msg
-			self.socket.send(f"Welcome back, {self.nick}!\r\n".encode())
+			self.send(f"Welcome back, {self.nick}!\r\n".encode())
 			self.server.broadcast(self.channel, f"{self.nick} has logged in\r\n", exclude=[self])
 			self.authed = True
 		else:
-			self.socket.send("Sorry, that is not the correct password.\r\n".encode())
+			self.send("Sorry, that is not the correct password.\r\n".encode())
 			
 
 	## Register a nick with a password
 	def register(self, msg):
 		if self.server.is_nick_registered(self.nick):
-			self.socket.send("This nickname is already registered\r\n".encode())
+			self.send("This nickname is already registered\r\n".encode())
 			return
 
 		if self.nick == "Anonymous":
-			self.socket.send("You must first select a nick name (/nick)\r\n".encode())
+			self.send("You must first select a nick name (/nick)\r\n".encode())
 			return
 
 
-		self.socket.send(self.server.rsaKey.publickey().exportKey())
+		self.send(self.server.rsaKey.publickey().exportKey())
 		salt = self.server.hasher.hash(Random.new().read(32))
 		self.server.register(self, self.server.hasher.hash(self.server.decryptor.decrypt(self.socket.recv(4096).strip()) + salt.encode()), salt)
 		self.authed = True
 
 	def claim(self, msg):
 		if not self.authed:
-			self.socket.send("You must be registered to claim a channel\r\n".encode())
+			self.send("You must be registered to claim a channel\r\n".encode())
 			return
 
 		if self.channel is None:
-			self.socket.send("You must be in a channel to claim it\r\n".encode())
+			self.send("You must be in a channel to claim it\r\n".encode())
+			return
+
+		if self.server.is_channel_claimed(self.channel):
+			if self.server.get_channel_owner(self.channel) == self.nick:
+				self.send("You already own this channel.\r\n".encode())
+			else:
+				self.send("This channel is already claimed.\r\n".encode())
+			return
 
 		self.server.claim_channel(self.nick, self.channel)
-		self.socket.send(f"{self.channel} is now registered to you, {self.nick}\r\n".encode())
+		self.send(f"{self.channel} is now registered to you, {self.nick}\r\n".encode())
+
+	def encrypt_channel(self, msg):
+		if self.channel is None:
+			self.send("You must be in a channel to encrypt it\r\n".encode())
+			return
+
+		if self.server.is_channel_claimed(self.channel):
+			if self.server.get_channel_owner(self.channel) == self.nick:
+				if msg.lower() == "true":
+					self.server.encrypt_channel(self.channel, True)
+				elif msg.lower() == "false":
+					self.server.encrypt_channel(self.channel, False)
+				else:
+					state = "" if self.server.is_channel_encrypted(self.channel) else "not "
+					self.send(f"/encrypt <true/false>\r\nCurrently this channel is {state}encrypted.\r\n".encode())
+			else:
+				self.send("You don't own this channel.\r\n".encode())
+		else:
+			self.send("This channel isn't registered.\r\n".encode())
 
 		
 
@@ -312,32 +369,32 @@ class Server:
 	def chat(self, client, msg):
 		for c in self.clients:
 			if (c.channel == client.channel): #Broadcast to everyone inside this channel
-				c.socket.send(f"<{client.nick}> {msg}\r\n".encode())
+				c.send(f"<{client.nick}> {msg}\r\n".encode())
 
 
 	def broadcast(self, channel, message, exclude=[]):
 		for c in self.clients:
 			if c not in exclude and c.channel == channel:
-				c.socket.send((message + "\r\n").encode())
+				c.send((message + "\r\n").encode())
 				
 	## Broadcast the arrival of someone
 	def arrival(self, client, channel):
 		if channel is None:
-			client.socket.send("Welcome to NARC Server 0.0.2!\r\n".encode())
+			client.send("Welcome to NARC Server 0.0.2!\r\n".encode())
 
 		if channel is not None and channel in self.channels: #If there is a MOTD currently set, send it
-			owner, motd = self.channels[channel]
-			client.socket.send(f"{motd}\r\n".encode())
+			owner, motd, _e = self.channels[channel]
+			client.send(f"{motd}\r\n".encode())
 
 		for c in self.clients:
 			if (c.channel != None and c.channel == channel): # Tell everyone that someone new connected
-				c.socket.send(f"Welcome {client.nick} to {channel}!\r\n".encode())
+				c.send(f"Welcome {client.nick} to {channel}!\r\n".encode())
 
 	## Broadcast the departure
 	def departure(self, client):
 		for c in self.clients:
 			if (c.channel != None and c.channel == client.channel and c != client): # Tell everyone that someone left
-				c.socket.send(f"{client.nick} has left {client.channel}..\r\n".encode())
+				c.send(f"{client.nick} has left {client.channel}..\r\n".encode())
 	
 	
 	def is_channel_claimed(self, channel):
@@ -348,21 +405,48 @@ class Server:
 		return False
 
 	def get_channel_owner(self, channel):
-		owner, motd = self.channels[channel]
+		owner, motd, _e = self.channels[channel]
 		return owner
 
 	def claim_channel(self, nick, channel):
-		self.channels[channel] = (nick, f"Welcome to {channel}")
+		self.channels[channel] = (nick, f"Welcome to {channel}", False)
 		self.save_channels()
+
+	def encrypt_channel(self, channel, encrypted):
+		if encrypted:
+			for client in self.clients:
+				if client.channel == channel:
+					if client.authed:
+						client.request_pubkey()
+					else:
+						client.send("Sorry, this channel has been encrypted and now requires you to be registered.\r\n".encode())
+						client.channel = None
+			owner, motd, _e = self.channels[channel]
+			self.channels[channel] = (owner, motd, True)
+		else:
+			for client in self.clients:
+				if client.channel == channel:
+					client.send("The channels encryption requirement has been disabled.\r\n".encode())
+			owner, motd, _e = self.channels[channel]
+			self.channels[channel] = (owner, motd, False)
+		self.save_channels()
+
+	def is_channel_encrypted(self, channel):
+		if channel not in self.channels:
+			return False
+		
+		owner, motd, encrypted = self.channels[channel]
+		return encrypted
 
 
 	## Set a channel's MOTD
-	def set_motd(self, client, motd): 
-		self.channels[client.channel] = (client.nick, motd)
+	def set_motd(self, client, motd):
+		owner, _m, _e = self.channels[client.channel] 
+		self.channels[client.channel] = (client.nick, motd, _e)
 		self.save_channels()
 
 	def get_motd(self, channel):
-		owner, motd = self.channels[channel]
+		owner, motd, _e = self.channels[channel]
 		return motd
 
 	## Remove a client
@@ -400,7 +484,7 @@ class Server:
 		
 		self.passwords[client.nick] = (password, salt)
 		self.save_passwords()
-		client.socket.send(f"Congratulations, {client.nick} is now registered to you.\r\n".encode())
+		client.send(f"Congratulations, {client.nick} is now registered to you.\r\n".encode())
 
 	def auth(self, nick, password):
 		(hash, salt) = self.passwords[nick]
