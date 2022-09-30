027e7342aef34fa639a27f3e12177f0d5896ef0e
Wyatt Marks <inbox@wyattmarks.com>
2022-09-29 21:17:15 -0500

    Redo look of client with rich library, also add chat history and password hiding

--- client.py
+++ client.py
@@ -12,6 +12,11 @@ from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto import Random
 from base64 import b64encode, b64decode
 
+from rich import print
+import time
+
+import readchar
+
 ## Client object for the client side
 #  Simply sends the commands you give it to the server
 #  Handles printing and getting input to make it more cohesive than just a telnet session
@@ -31,12 +36,109 @@ class Client:
 		self.channel_encrypted = False
 
 		self.aes_key = None
+		self.history = []
+
+		self.color_names = {"Anonymous": '[#000000]'}
 		
+	def get_input(self, prefix=""):
+		sys.stdout.write(prefix)
+		cursor = 0
+		msg = ''
+		history_position = -1
+		c = ''
+		while c != readchar.key.ENTER:
+			c = readchar.readkey()
+
+			if c == readchar.key.UP:
+				history_position += 1
+				if history_position < len(self.history):
+					self.print_prefix()
+					print(self.history[len(self.history) - history_position - 1], end='')
+					cursor = len(self.history[len(self.history) - history_position - 1])
+				else:
+					history_position = len(self.history) - 1
+				continue
+			
+			if c == readchar.key.DOWN:
+				history_position -= 1
+				self.print_prefix()
+				if history_position > -1:
+					print(self.history[len(self.history) - history_position - 1], end='')
+					cursor = len(self.history[len(self.history) - history_position - 1])
+				else:
+					history_position = -1
+					print(msg, end='')
+					cursor = len(msg)
+				continue
+
+			if c == readchar.key.LEFT:
+				cursor -= 1
+				if cursor < 0:
+					cursor = 0
+				else:
+					sys.stdout.write(c)
+				continue
+
+			if c == readchar.key.RIGHT:
+				cursor += 1
+				selected = msg if history_position == -1 else self.history[len(self.history) - history_position - 1]
+				if cursor > len(selected):
+					cursor = len(selected)
+				else:
+					sys.stdout.write(c)
+				continue
+
+			if history_position != -1:
+				msg = self.history[len(self.history) - history_position - 1]
+				history_position = -1
+
+			if c == readchar.key.ENTER:
+				continue
+
+			if c == readchar.key.BACKSPACE:
+				if cursor > 0:
+					msg = msg[:cursor-1] + msg[cursor:]
+					cursor -= 1
+					sys.stdout.write('\b \b' + msg[cursor:] + ' ')
+					for i in range(len(msg[cursor:]) + 1):
+						sys.stdout.write(readchar.key.LEFT)
+					sys.stdout.flush()
+				continue
+			elif self.waiting_for_password:
+				sys.stdout.write('*')
+			else:
+				sys.stdout.write(c + msg[cursor:])
+				for i in range(len(msg[cursor:])):
+					sys.stdout.write(readchar.key.LEFT)
+			msg = msg[:cursor] + c + msg[cursor:]
+			cursor += 1
+			sys.stdout.flush()
+
+		if not self.waiting_for_password:
+			self.history.append(msg)
+
+		return msg
+
 
 	def connect(self):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.connect((self.ip, self.port))
 
+	def register_nick(self, nick):
+		if nick not in self.color_names:
+			import random
+			self.color_names[nick] = f"[#{random.randrange(0x1000000):06x}]"
+
+	def print_prefix(self):
+		sys.stdout.write('\033[2K\033[1G')
+		if self.channel is not None:
+			if self.channel_encrypted:
+				print(f"[[dodger_blue2]{self.channel}[white]]",end='')
+				print(f" <{self.color_names[self.nick]}{self.nick}[white]>: ", end="")
+			else:
+				print(f"\[{self.channel}] <{self.color_names[self.nick]}{self.nick}[white]>: ", end="")
+		else:
+			print(f'<{self.color_names[self.nick]}{self.nick}[white]>: ', end='')
 
 	def print_responses(self):
 		import sys
@@ -68,32 +170,41 @@ class Client:
 						self.waiting_for_password = True
 						sys.stdout.write('\033[2K\033[1G')
 						print("Password: ", end="")
-						sys.stdout.flush()
 						continue
 
-				if response.startswith("PUBKEYREQ"):
-					self.socket.send(self.rsaKey.publickey().exportKey())
-					continue
-
 				for response in response.splitlines():
+					if response.startswith("PUBKEYREQ"):
+						self.socket.send(self.rsaKey.publickey().exportKey())
+						continue
 					
 					if response.startswith(f"{self.nick} is now known as ") or response.startswith("Welcome back, "):
 						self.nick = response.strip().replace(f"{self.nick} is now known as ", "").replace("Welcome back, ", "")
 						self.nick = self.nick[0:len(self.nick)-1]
+						self.register_nick(self.nick)
 
 					if response.startswith("You are now known as "):
 						self.nick = response.strip().replace("You are now known as ", "")
 						self.nick = self.nick[0:len(self.nick)-1]
-						sys.stdout.write('\033[2K\033[1G') #Get rid of the <user> in console from the input() call
-						print(f"<{self.nick}> ", end="")
-						sys.stdout.flush()
+						self.register_nick(self.nick)
+						self.print_prefix()
 						continue
 
-					if not response.strip().startswith(f"<{self.nick}> "): #The way input() works, we'll already see our chat messages. Therefore don't print them again
-						sys.stdout.write('\033[2K\033[1G') #Get rid of the <user> in console from the input() call
-						print(response.strip())
-						print(f"<{self.nick}> ", end="") #put the <user> back so it looks right
-						sys.stdout.flush() #flush stdout so that <user> actually appears
+					if response.startswith("Joining #"):
+						self.channel = "#" + response.replace("Joining #", "").replace("...", "")
+						self.channel_encrypted = False
+
+					if response.startswith("This channel is encrypted with AES."):
+						self.channel_encrypted = True
+
+					if response.startswith("<"):
+						self.register_nick(response[1:response.find('>')])
+						response = "<" + self.color_names[response[1:response.find('>')]] + response[1:response.find('>')] +"[white]>" + response[response.find('>') + 1:]
+					
+					sys.stdout.write('\033[2K\033[1G')
+					sys.stdout.flush()
+					print(time.strftime('[%H:%M:%S] ') + response)
+				self.print_prefix()
+
 			except socket.error:
 				sys.stdout.write('\033[2K\033[1G') #Get rid of the <user> in console from the input() call
 				print("Connection lost, attempting to reconnect..")
@@ -108,8 +219,6 @@ class Client:
 						sleep(5)
 				print("Connected!")
 				self.__init__(self.ip, self.port) #reset everything
-				print(f"<{self.nick}> ", end="") 
-				sys.stdout.flush()
 
 	def send(self, message):
 		if self.channel_encrypted:
@@ -133,17 +242,14 @@ class Client:
 	def handle_input(self):
 		while True:
 			try:
-				message = input(f"<{self.nick}> ")
+				self.print_prefix()
+				message = self.get_input()
 				if len(message) > 0:
 					if self.waiting_for_password:
 						password = self.encryptor.encrypt(message.encode())
 						self.socket.send(password)
 						self.waiting_for_password = False
 					elif message.startswith("/"): #If it starts with / then it is a command, and we should not prepend /chat
-						if message.startswith("/join #"):
-							self.channel = message.replace("/join ", "")
-							self.channel_encrypted = False
-
 						self.send(f"{message} \r\n".encode())
 
 						if message.startswith("/quit") or message.startswith("/exit"):
@@ -159,20 +265,20 @@ class Client:
 if __name__ == "__main__":
 	import sys
 
-	server = "127.0.0.1"
-	if len(sys.argv) != 2:
-		print('You must give a server address as the only argument')
-		exit(0) #comment out for local testing
-	else:
+	server = "localhost"
+	port = 0xBEEF
+	if len(sys.argv) >= 2:
 		server = sys.argv[1]
+	if len(sys.argv) >= 3:
+		port = sys.argv[2]
 
 
-	c = Client(server, 0xBEEF)
+	c = Client(server, port)
 
 	try:
 		c.connect()
 	except ConnectionRefusedError:
-		print(f"Server not running on {server}")
+		print(f"Unable to connect to {server}")
 		exit(0)
 
 	print_thread = threading.Thread(target=c.print_responses) #Put this in a thread so the socket.recv call doesn't block us from sending input
